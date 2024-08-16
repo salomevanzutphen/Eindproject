d71e6d3e9c45b48d61e8fe8dc9448a8cca583ee1
salomevanzutphen <vanzutphensalome@gmail.com>
2024-08-16 17:07:03 +0200

    Homepagina + verwijzing naar about pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
@@ -8,21 +7,22 @@ import AboutUs from "./pages/AboutUs/AboutUs.jsx";
 import MySync from "./pages/MySync/MySync.jsx";
 import Blog from "./pages/Blog/Blog.jsx";
 import CreatePost from "./pages/CreatePost/CreatePost.jsx";
-import Profile from "./pages/Profile/Profile.jsx";
 import Connect from "./pages/Connect/Connect.jsx";
+import ScrollToTop from './helpers/ScrollToTop.jsx'; // Import the ScrollToTop component
+import EditPost from "./pages/EditPost/EditPost.jsx";
 
 function App() {
-
     return (
         <>
             <Navbar />
+            <ScrollToTop />  {/* Add the ScrollToTop component here */}
             <div className="app">
                 <Routes>
                     {/* Public Routes */}
                     <Route path="/" element={<Home />} />
+                    <Route path="/edit-post/:id" element={<EditPost />} />
                     <Route path="/aboutus" element={<AboutUs />} />
                     <Route path="/blog" element={<Blog />} />
-                    <Route path="/profile" element={<Profile />} />
                     <Route path="/mysync" element={<MySync />} />
                     <Route path="/create-post" element={<CreatePost />} />
                     <Route path="/connect" element={<Connect />} />
@@ -34,5 +34,3 @@ function App() {
 }
 
 export default App;
-
-
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,23 +10,23 @@ function Home() {
         <div className="home-page-wrapper">
             <section className="home-part1">
                 <img className="background-image1" src={mainimage} alt="background-image"/>
-                <p>Transform your Health, < br/> Connect to your Cycle’s Rhythm</p>
+                <p className="homepage-tagline">Transform your Health, < br/> Connect to your Cycle’s Rhythm</p>
             </section>
 
             <section className="home-part2">
-                <img className="intro-image" src="https://i.pinimg.com/736x/87/2d/b7/872db7503d2347cc15483e31b70413f7.jpg" alt="Profileshot"/>
+                <img className="home-profile-image" src="https://i.pinimg.com/736x/87/2d/b7/872db7503d2347cc15483e31b70413f7.jpg" alt="Profileshot"/>
                 <div className="home-part2-info">
                     <p className="homepage-quote"> 'My mother's health issues were a wake-up call for me.
                     Now I strive to be a role-model for those who want to also
                     I'm not sure what I'm writing here but im trying my best hahah' </p>
 
                     <div className="home-part2-images">
-                        <img className="home-image" src="https://i.pinimg.com/564x/ca/bc/30/cabc30dc52bcb870cb5a6af69d603439.jpg" alt="home-image"/>
-                        <img className="home-image" src="https://i.pinimg.com/564x/04/cd/71/04cd718725f9d5878375b72e007bdc89.jpg" alt="home-image"/>
-                        <img className="home-image" src="https://i.pinimg.com/564x/20/3a/c8/203ac84719f087f7da3ff51a6fa718c0.jpg" alt="home-image"/>
+                        <img className="home-image" src="https://i.pinimg.com/564x/5f/7f/70/5f7f70b54ee07e1a90f5a10cc7e66474.jpg" alt="home-image"/>
+                        <img className="home-image" src="https://i.pinimg.com/564x/c1/fe/f6/c1fef632beabb5417911744c7c2d0c2a.jpg" alt="home-image"/>
+                        <img className="home-image" src="https://i.pinimg.com/736x/cf/f7/2e/cff72e1067f3e8c14cc3d762afa34e76.jpg" alt="home-image"/>
                     </div>
 
-                    <h2>Cycle-synced living</h2>
+                    <p className="cycle-synced">Cycle-synced living</p>
                     <p className="cycleliving-p">Find out what inspired me < br/> to create this application</p>
                     <button className="mystory" onClick={() => navigate('/aboutus')}>Read my story</button>
                 </div>
