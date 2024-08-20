294e97c7fac0158cc88dcf65ef57405d729f1073
salomevanzutphen <vanzutphensalome@gmail.com>
2024-08-20 23:34:02 +0200

    making sure the button doesn't move & adding more prompts

--- src/components/Journalprompt/JournalPrompt.jsx
+++ src/components/Journalprompt/JournalPrompt.jsx
@@ -4,19 +4,21 @@ import "./JournalPrompt.css";
 
 const JournalPrompts = () => {
     const prompts = [
-        "What coping mechanism do you opt for when you feel stressed? Could you improve this?",
-        "Think of 3 people that are influencing your life positively at the moment and why.",
-        "How do you stay motivated during challenging times, and what inspires you to keep going?",
-        "What triggers emotions in you? What are you most sensitive to?",
-        "What did you learn most from the people who raised you?",
-        "How do you nurture your relationships?",
-        "How can the people around you make you feel heard & appreciated? Have you communicated this to them / are they aware of this?",
-        "Reflect on the past year. What are the biggest hurdles that are repeatedly in your way? Improvise on a new approach that could help you tackle it better.",
-        "What are the 5 most important goals on your vision board?",
-        "What is your definition of a balanced routine? Draw out your ideal weekly schedule.",
+        "What coping mechanism do you opt for when you feel stressed? Is it healthy for you?",
+        "Do the important people in your life know how to nurture and be there for you?",
+        "What is your communication style when you feel upset? Do you know how to express yourself effectively when you're not feeling your best?",
+        "Reflect on the past year. What (unexpected) changes made you grow and how?",
+        "What are the 5 most important goals on your vision board? Why are these goals important for you, what motivates you?",
+        "What is your definition of a balanced routine? Draw out your ideal weekly schedule and compare it to your current lifestyle.",
         "When was the last time you had some me-time?",
-        "Think of a time where you betrayed your boundaries or morals in order to be accepted by the group. What unique qualities make you different? What kind of community would people with similar qualities be in?",
-        "Reflect on an achievement that you are proud of. Did you doubt yourself a lot in the process? Would you say you have a good idea of your capabilities or do you need more self-trust?"
+        "When do you feel most like yourself?",
+        "What has been the hardest thing to forgive yourself for?",
+        "What risks are you scared of taking, what are you afraid to lose?",
+        "What are your core values? Do your choices align with them?",
+        "When do you feel most confident?",
+        "When did you last feel insecure?",
+        "What song do you identify with mostly during this time?",
+        "What would you do if you knew you could not fail?"
     ];
 
     const getRandomPrompt = () => {
@@ -36,7 +38,7 @@ const JournalPrompts = () => {
 
     return (
         <div className="journalprompts">
-            <h3>Your journal inspo:</h3>
+            <h3>Your Journal Inspo:</h3>
             <p>{currentPrompt}</p>
             <button className="journal-button" onClick={shufflePrompt}>Shuffle</button>
         </div>
