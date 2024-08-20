6b19bd4b050f4a1ad327f589d83f626236de8d8c
salomevanzutphen <vanzutphensalome@gmail.com>
2024-08-20 23:35:14 +0200

    new homepage styling (part 2)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,22 +14,31 @@ function Home() {
             </section>
 
             <section className="home-part2">
-                <img className="home-profile-image" src="https://i.pinimg.com/736x/87/2d/b7/872db7503d2347cc15483e31b70413f7.jpg" alt="Profileshot"/>
-                <div className="home-part2-info">
-                    <p className="homepage-quote"> 'My mother's health issues were a wake-up call for me.
-                    Now I strive to be a role-model for those who want to also
-                    I'm not sure what I'm writing here but im trying my best hahah' </p>
-
-                    <div className="home-part2-images">
-                        <img className="home-image" src="https://i.pinimg.com/564x/5f/7f/70/5f7f70b54ee07e1a90f5a10cc7e66474.jpg" alt="home-image"/>
-                        <img className="home-image" src="https://i.pinimg.com/564x/c1/fe/f6/c1fef632beabb5417911744c7c2d0c2a.jpg" alt="home-image"/>
-                        <img className="home-image" src="https://i.pinimg.com/736x/cf/f7/2e/cff72e1067f3e8c14cc3d762afa34e76.jpg" alt="home-image"/>
+                <p className="cycle-synced">Cycle-synced living</p>
+                <p className="homepage-subtitle">Finding Balance Through</p>
+                <div className="home-part2-pillars">
+                    <div className="pillar">
+                    <img className="home-image" src="https://i.pinimg.com/564x/d6/b8/cc/d6b8cc9a742d178c5502d3c65f0b75ed.jpg" alt="home-image"/>
+                     <h3>Nutrition</h3>
                     </div>
 
-                    <p className="cycle-synced">Cycle-synced living</p>
-                    <p className="cycleliving-p">Find out what inspired me < br/> to create this application</p>
+                    <div className="pillar">
+                        <img className="home-image" src="https://i.pinimg.com/564x/91/c5/7b/91c57b6b899c8109e4db1ec811026176.jpg" alt="home-image"/>
+                        <h3>Mindfulness</h3>
+                    </div>
+
+                    <div className="pillar">
+                    <img className="home-image" src="https://i.pinimg.com/564x/3a/48/58/3a4858185b87780de49e4dbd83b810d6.jpg" alt="home-image"/>
+                        <h3>Movement</h3>
+                    </div>
+               </div>
+
+                    <p className="homepage-quote"> 'My mother's health issues were a wake-up call for me.
+                    Now I strive to be a role-model for those who want to also
+                    I'm not sure what I'm writing here but im trying my best hahah'<br/> - Salomé, Founder </p>
+
                     <button className="mystory" onClick={() => navigate('/aboutus')}>Read my story</button>
-                </div>
+
             </section>
         </div>
     );
