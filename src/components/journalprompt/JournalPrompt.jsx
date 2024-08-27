ea68097541437d2778363a12cefd86fe041170b1
salomevanzutphen <vanzutphensalome@gmail.com>
2024-08-27 20:04:45 +0200

    update with new prompts

--- src/components/journalprompt/JournalPrompt.jsx
+++ src/components/journalprompt/JournalPrompt.jsx
@@ -4,19 +4,19 @@ import "./JournalPrompt.css";
 
 const JournalPrompt = () => {
     const prompts = [
-        "What coping mechanism do you opt for when you feel stressed? Could you improve this?",
-        "Think of 3 people that are influencing your life positively at the moment and why.",
-        "How do you stay motivated during challenging times, and what inspires you to keep going?",
-        "What triggers emotions in you? What are you most sensitive to?",
+        "What coping mechanism do you opt for when you feel stressed?",
         "What did you learn most from the people who raised you?",
-        "How do you nurture your relationships?",
-        "How can the people around you make you feel heard & appreciated? Have you communicated this to them / are they aware of this?",
-        "Reflect on the past year. What are the biggest hurdles that are repeatedly in your way? Improvise on a new approach that could help you tackle it better.",
         "What are the 5 most important goals on your vision board?",
-        "What is your definition of a balanced routine? Draw out your ideal weekly schedule.",
-        "When was the last time you had some me-time?",
-        "Think of a time where you betrayed your boundaries or morals in order to be accepted by the group. What unique qualities make you different? What kind of community would people with similar qualities be in?",
-        "Reflect on an achievement that you are proud of. Did you doubt yourself a lot in the process? Would you say you have a good idea of your capabilities or do you need more self-trust?"
+        "What is your definition of a balanced routine?",
+        "What do you want to be known for in this life?",
+        "How do you handle criticism or feedback?",
+        "What are your core values?",
+        "What boundaries do you set to protect your well-being?",
+        "What are the lessons you've learned from your biggest failures?",
+        "What are your strategies for staying resilient in tough situations?",
+        "What does living authentically mean to you?",
+        "Describe a time when you stepped out of your comfort zone. What did you learn?",
+        "When do you feel most like yourself?"
     ];
 
     const getRandomPrompt = () => {
