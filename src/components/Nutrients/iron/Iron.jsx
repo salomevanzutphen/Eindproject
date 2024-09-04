8700f55c3fcf6287050a53ce58f596b0c745b51b
salomevanzutphen <vanzutphensalome@gmail.com>
2024-09-04 22:28:20 +0200

    phase 1 copytekst & nutrient adjustment

--- src/components/Nutrients/iron/Iron.jsx
+++ src/components/Nutrients/iron/Iron.jsx
@@ -11,7 +11,7 @@ function Iron({ backgroundColorClass, description, showDescription, onClick }){
                 <div className="iron-description">
                     <div className="iron-description-content">
                         <p className="iron-intro">
-                        Menstruation can lead to the loss of iron-rich blood. Iron is a critical component of hemoglobin, the protein in red blood cells responsible for transporting oxygen throughout the body. This supports energy levels and overall vitality, deficiency in iron can lead to fatigue and weakness.
+                            {description}
                     </p>
 
                     </div>
